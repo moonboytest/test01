c3655665e7920156538a4181ebfeb6648b255334
moonboytest <moonboy4131@gmail.com>
2022-11-20 17:47:47 +0400

    test

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 //Список подозреваемых
-var Suspects = []string{"Roman", "Sergay", "Sasha", "Matvey", "Bobr"}
+var Suspects = []string{"Roman", "Sergay", "Sasha", "Matvey", "Bobrb"}
 
 func main() {
 	fmt.Println(search.FindTheCriminal(db.Peoples, Suspects))
